d325c8e48199355532661be938d7b2ca5b15f56d
ByoungJinBae <30740720+ByoungJinBae@users.noreply.github.com>
2017-08-05 12:27:17 -0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
-    public static void main(String[] ergs) {
+    public static void main(String[] args) {
     	System.out.println("Hi World");
     	System.out.println(123);
     }
